e5f8a8ae221c149a8d3ad4566f0d30014f1cea0f
Karma <karmagrg23@gmail.com>
2024-11-08 09:11:00 -0500

    fix metrics

--- portfolio.py
+++ portfolio.py
@@ -52,8 +52,7 @@ def main():
     ## filteer chnage positive sort by chg
     winnnerDf = round(df.sort_values(by='chg',ascending=False),1)[df.chg>0]
     # filter chnage negative sort by chg
-    losserDf = round(df.sort_values(by='chg',ascending=False),1)[df.chg<0]
-    #winnnerDf = round(df.sort_values(by='chg',ascending=False),1)
+    losserDf = round(df.sort_values(by='chg'),1)[df.chg<0]
     
     ###
     dataset = {}
@@ -67,23 +66,18 @@ def main():
     for col in df_asset.columns:
         df_asset[col] = df_asset[col].apply(lambda x: (x / df_asset[col].head(1).values[0])-1)        
     
-    
-    #st.dataframe(df)
     with st.container(border=True):
-        
+        # Metrics
         with st.container(border=True):
             col1, col2,col3,col4 = st.columns(4)
             with col1:
                 st.metric("Total Return","${:,.0f}".format(netValue),delta="{:,.2f}%".format((netValue/totalCost)*100))
-            #with col2:
-            #    st.metric("YTD Return","{:.1f}%".format(df_asset['ASSETS'].tail(1).values[0]*100), delta="SPY {:.1f}%".format(df_asset['SPY'].tail(1).values[0]*100))
             with col2:
                 st.metric("Daily change","${:.0f}".format(princeChange), delta="{:.2f}%".format(dailyChange))
             with col3:
                 st.metric("Top Gain", winnnerDf.head(1).ticker.values[0] ,delta=str(winnnerDf.head(1).chg.values[0])+"%")
             with col4:
                 st.metric("Top Loss", losserDf.head(1).ticker.values[0] ,delta=str(losserDf.head(1).chg.values[0])+"%")
-        
         ## winner and loser
         with st.container(border=True):
             col1, col2 = st.columns(2)
@@ -95,29 +89,28 @@ def main():
                 st.subheader(f":red[Losers:] ",divider='red')
                 bottom5 = [f"{row['ticker']}: {row['chg']}%" for index, row in losserDf.sort_values(by='chg').head(3).iterrows()]
                 st.write(f":red[{bottom5}]")
-        
         ## Portfolio to Benchmark
-        st.subheader(f'**YTD: :blue[Portfolio] ({round(df_asset.ASSETS.iloc[-1]*100,1)}%) :blue[vs] :gray[SPY] ({round(df_asset.SPY.iloc[-1]*100,1)}%)**',divider=True)
-        #plot line chart
-        st.line_chart(data=df_asset*100.00,color=["#67BCF5", "#7D7D85"], use_container_width=True)
-        
+        with st.container(border=True):
+            st.subheader(f'**YTD: :blue[Portfolio] ({round(df_asset.ASSETS.iloc[-1]*100,1)}%) :blue[vs] :gray[SPY] ({round(df_asset.SPY.iloc[-1]*100,1)}%)**',divider=True)
+            #plot line chart
+            st.line_chart(data=df_asset*100.00,color=["#67BCF5", "#7D7D85"], use_container_width=True)
         # tree of portfolio
-        st.subheader('Allocation and Daily Changes 🔺🔻',divider=True)
-        fig1= px.treemap(df, path=['country','sector','ticker'],values='value', color='chg',hover_name='ticker',color_continuous_scale='RdYlGn',hover_data=['ticker', 'chg', 'country','sector']) #RdYlBu
-        # Optionally format hover_data to show change in a more readable way
-        fig1.update_traces(hovertemplate="Ticker: %{hovertext}<br>Change: %{customdata[1]:.1f}%<br>Country: %{customdata[2]}<br>Sector: %{customdata[3]}")
-        fig1.update_layout(margin=dict(l=0, r=0, t=10, b=50))
-        st.plotly_chart(fig1, use_container_width=True)
-               
+        with st.container(border=True):
+            st.subheader('Allocation and Daily Changes 🔺🔻',divider=True)
+            fig1= px.treemap(df, path=['country','sector','ticker'],values='value', color='chg',hover_name='ticker',color_continuous_scale='RdYlGn',hover_data=['ticker', 'chg', 'country','sector']) #RdYlBu
+            # Optionally format hover_data to show change in a more readable way
+            fig1.update_traces(hovertemplate="Ticker: %{hovertext}<br>Change: %{customdata[1]:.1f}%<br>Country: %{customdata[2]}<br>Sector: %{customdata[3]}")
+            fig1.update_layout(margin=dict(l=0, r=0, t=10, b=50))
+            st.plotly_chart(fig1, use_container_width=True)       
         # plot my holdings line chart
-        st.subheader('YTD Return',divider='rainbow')
-        myHoldings = df_raw[tickers].copy()
-        for col in myHoldings.columns:
-            myHoldings[col] = myHoldings[col].apply(lambda x: round(x / myHoldings[col].head(1).values[0],4)-1)*100
-        #st.dataframe(myHoldings)
-        st.line_chart(myHoldings)        
-        
+        with st.container(border=True):
+            st.subheader('YTD Return',divider='rainbow')
+            myHoldings = df_raw[tickers].copy()
+            for col in myHoldings.columns:
+                myHoldings[col] = myHoldings[col].apply(lambda x: round(x / myHoldings[col].head(1).values[0],4)-1)*100
+            #st.dataframe(myHoldings)
+            st.line_chart(myHoldings)        
         ## my holdings table
-        with st.expander("MY Holdings Positions",icon="🚨"):
+        with st.expander(":blue[View Positions by Daily Change %]",icon="🚨"):
             st.subheader('Positions by Daily Change %',divider=True)
             st.dataframe(df.drop(columns=['shares','return']).sort_values(by='chg'),use_container_width=True,hide_index=True) #prints tickers dataframe   
